38400a0a866fe918bd238f75d97d7b015c47b2bb
Bilel KEDIDI <bilel.kedidi@gmail.com>
2015-01-09 00:33:18 +0100

    CF

--- lib/patch/issues_controller_patch.rb
+++ lib/patch/issues_controller_patch.rb
@@ -7,6 +7,7 @@ module  Patch
       base.send(:include, InstanceMethods)
       base.class_eval do
        after_filter  :update_version, :only=>[:update]
+       after_filter  :update_versions, :only=>[:bulk_update]
         #alias_method_chain :issue_list, :order
       end
     end
@@ -16,14 +17,26 @@ module  Patch
 
   module InstanceMethods
     private
-    def update_version
-      cf = CustomValue.where("customized_type= 'Project' and custom_field_id= 36").where(customized_id: @issue.project.id)
+    def update_versions
+      if @issues.any?
+        @issues.each do |issue|
+          update_issue_version(issue)
+        end
+      end
+    end
 
+    def update_version
+      update_issue_version(@issue)
+    end
+
+    def update_issue_version(issue)
+
+      cf = CustomValue.where("customized_type= 'Project' and custom_field_id= 36").where(customized_id: issue.project.id)
       if cf.present? and cf.first.value == "1"
-        version_changed = @issue.journals.last(1).map(&:details).flatten.select{|d| d.prop_key == "fixed_version_id" }
+        version_changed = issue.journals.last(1).map(&:details).flatten.select{|d| d.prop_key == "fixed_version_id" }
         if version_changed.any?
-          children = @issue.children
-          loop_update_version_child(children, @issue)if children.any?
+          children = issue.children
+          loop_update_version_child(children, issue) if children.any?
         end
       end
     end
