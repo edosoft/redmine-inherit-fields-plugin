6881aa83c9562f2079e12e199d221a7d00d13515
Bilel KEDIDI <bilel.kedidi@gmail.com>
2015-02-09 13:08:52 +0100

    updates

--- lib/issues_update_hook.rb
+++ lib/issues_update_hook.rb
@@ -2,12 +2,25 @@ class Hooks < Redmine::Hook::ViewListener
   def controller_issues_edit_before_save(context={})
     issue = context[:issue]
     settings = Setting.send("plugin_redmine_subtasks_inherited_fields") || {}
+
+    new_parent_id = context[:params][:issue][:parent_issue_id].to_i
+    if new_parent_id != issue.parent_id
+      parent_task = Issue.find(new_parent_id) if issue.parent_id
+      update_subtask(issue, parent_task, settings, false) if parent_task
+    end
     update_all_children_subtasks(issue, settings)
   end
 
   def controller_issues_bulk_edit_before_save(context={})
     issue = context[:issue]
     settings = Setting.send("plugin_redmine_subtasks_inherited_fields") || {}
+
+    new_parent_id = context[:params][:issue][:parent_issue_id].to_i
+    if new_parent_id != issue.parent_id
+      parent_task = Issue.find(new_parent_id) if issue.parent_id
+      update_subtask(issue, parent_task, settings, false) if parent_task
+    end
+
     update_all_children_subtasks(issue, settings)
   end
 
@@ -19,7 +32,7 @@ class Hooks < Redmine::Hook::ViewListener
     end
   end
 
-  def update_subtask(sub_task, issue, settings)
+  def update_subtask(sub_task, issue, settings, can_save = true)
     sub_task.fixed_version_id = issue.fixed_version_id if settings[:inherit_fixed_version_id]
     sub_task.category_id = issue.category_id if settings[:inherit_category_id]
     sub_task.assigned_to_id = issue.assigned_to_id if settings[:inherit_assigned_to_id]
@@ -29,7 +42,7 @@ class Hooks < Redmine::Hook::ViewListener
     sub_task.description = issue.description if settings[:inherit_description]
     sub_task.is_private = issue.is_private if settings[:inherit_is_private]
     sub_task.status_id = issue.status_id if settings[:inherit_status_id]
-    sub_task.save
+    sub_task.save if can_save
   end
 
 end
